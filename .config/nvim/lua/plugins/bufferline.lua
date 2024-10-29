41c8ff0ad847be5c792b80c1f81d3a44ace293c9
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2024-10-29 08:37:52 -0700

    bufferline fix options

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,11 @@
 return {
   {
     "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
     keys = {
       { "gb", "<cmd>BufferLinePick<CR>", desc = "Pick Buffer" },
     },
