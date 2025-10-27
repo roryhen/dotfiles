fc8a7d469c8d6555f85f0be28136a1d57b6f0d37
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2025-10-26 21:16:56 -0700

    simplify config

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,4 +0,0 @@
-local set = vim.keymap.set
-
--- Source nvim
-set("n", "<leader>sv", "<cmd>luafile $MYVIMRC<cr>", { desc = "Source nvim config" })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,6 @@ local opt = vim.opt
 
 opt.wrap = true
 
-opt.scrolloff = 10
+opt.scrolloff = 20
 
 vim.g.lazyvim_prettier_needs_config = false
