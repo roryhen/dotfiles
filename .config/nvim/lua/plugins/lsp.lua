84f0f9a7528b64e39dd07bb73ef1767b1b80afb6
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2025-12-24 08:43:29 -0800

    deno config, updates

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,12 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
+        denols = {
+          enabled = true,
+          root_dir = function()
+            return require("lspconfig").util.root_pattern({ "deno.json", "deno.jsonc", "deno.lock" })
+          end,
+        },
         emmet_language_server = {},
         --- https://github.com/yioneko/vtsls/blob/main/packages/service/configuration.schema.json
         vtsls = {
