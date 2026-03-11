eb41617fb813018b885e66027716abb16856953c
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2026-03-11 09:09:54 -0700

    add back in wezterm config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -20,4 +20,15 @@ return {
       },
     },
   },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    dependencies = { "DrKJeff16/wezterm-types" },
+    opts = {
+      library = {
+        -- Other library configs...
+        { path = "wezterm-types", mods = { "wezterm" } },
+      },
+    },
+  },
 }
