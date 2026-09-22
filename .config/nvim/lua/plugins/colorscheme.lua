cbefbe5fabb176cd57502a2b97d2fd0efa00f52f
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2026-09-22 09:55:00 -0700

    restore theme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "bluz71/vim-nightfly-colors",
+    enabled = false,
+    name = "nightfly",
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    -- "nightfox", "duskfox", "nordfox" "terafox", "carbonfox"
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
